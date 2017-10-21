55ded40c2abf10860c31fcef7631d2c8af969499
Brent253 <brent4994@hotmail.com>
2017-10-21 15:34:15 -0700

    Updated android homework

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
@@ -1,10 +1,9 @@
 package co.miniforge.corey.stringanalyzer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class AnalyzerActivity extends AppCompatActivity {
